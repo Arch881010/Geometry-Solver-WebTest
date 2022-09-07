f54a78373cbf77110829173d3e43c36e2a879e73
Arch881010 <111789310+Arch881010@users.noreply.github.com>
2022-09-07 13:01:21 -0500

    Yes

--- primary.js
+++ primary.js
@@ -2,6 +2,8 @@ document.getElementById("submit").onclick = function () {
     if(document.getElementById("math-type").value == "distance"){
         alert("1")
     }else if(document.getElementById("math-type").value == "peremeter"){
-        alert("2")
+        var x = document.createElement("input");
+        x.setAttribute("style", "text");
+        document.body.appendChild(x);
     }
 }
