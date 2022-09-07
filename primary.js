158eedcad69905f9c19319e6ccabf35904a1cb94
Arch881010 <111789310+Arch881010@users.noreply.github.com>
2022-09-07 13:17:05 -0500

    New Test

--- primary.js
+++ primary.js
@@ -1,9 +1,21 @@
 document.getElementById("submit").onclick = function () {
     if(document.getElementById("math-type").value == "distance"){
-        alert("1")
+        if(b1b2 === true) {
+        } else {
+            
+        }
     }else if(document.getElementById("math-type").value == "peremeter"){
-        var x = document.createElement("input");
-        x.setAttribute("style", "text");
-        document.body.appendChild(x);
+        if(b1b2 === true) {
+        } else {
+            var b1 = document.createElement("input");
+            b1.setAttribute("style", "text");
+            b1.setAttribute("id", "b1");
+            document.body.appendChild(b1);
+            var b2 = document.createElement("input");
+            b2.setAttribute("style", "text");
+            b2.setAttribute("id", "b2");
+            document.appendChild(b2);
+            var b1b2 = true;
+        }
     }
 }
