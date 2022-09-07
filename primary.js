923f049818384b59a784db4bc020e3aa0ea07579
Arch881010 <111789310+Arch881010@users.noreply.github.com>
2022-09-07 12:29:03 -0500

    Test

--- primary.js
+++ primary.js
@@ -1,7 +1,7 @@
 document.getElementById("submit").onclick = function () {
     if(document.getElementById("math-type").value == "distance"){
-
+        alert("1")
     }else if(document.getElementById("math-type").value == "peremeter"){
-        
+        alert("2")
     }
 }
