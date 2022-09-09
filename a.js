24668bed25a22a4de2ff11ede674b69a659025f3
Arch881010 <111789310+Arch881010@users.noreply.github.com>
2022-09-09 12:04:32 -0500

    More updates

--- a.js
+++ a.js
@@ -2,9 +2,9 @@
 document.getElementById("submit").onclick = function () {
     // SPACE
     var br = document.createElement("br"); //BR 
-    var t1 = document.createElement("text"); //T1 / X1
-    var t2 = document.createElement("text");
-    var b1 = document.createElement("input");
+    var tx1 = document.createElement("text"); //Tx1 / X1
+    var ty1 = document.createElement("text"); //Ty1 / Y1
+    var b1 = document.createElement("input"); 
     var b1b = document.createElement("input");
     var b2 = document.createElement("input");
     var b2b = document.createElement("input");
@@ -13,24 +13,24 @@ document.getElementById("submit").onclick = function () {
     //BR APPEND AND ID
     document.body.appendChild(br); 
     br.setAttribute("id", "br");
-    //T1 ID + STYLE
-    t1.setAttribute("stye", "text");
-    t1.setAttribute("id", "t1");
+    //Tx1 ID + STYLE
+    tx1.setAttribute("stye", "text");
+    tx1.setAttribute("id", "tx1");
     //B1 ID + STYLE
     b1.setAttribute("id", "b1");
     b1.setAttribute("style", "text");
     //B1B ID + STYLE
     b1b.setAttribute("id", "b1b");
     b1b.setAttribute("style", "text");
-    //T2 ID + STYLE
-    t2.setAttribute("stye", "text");
-    t2.setAttribute("id", "t2");
+    //Ty1 ID + STYLE
+    ty1.setAttribute("stye", "text");
+    ty1.setAttribute("id", "ty1");
     //B2 ID + STYLE
     b2.setAttribute("style", "text");
     b2.setAttribute("id", "b2");
     //B2B ID + STYLE
     b2b.setAttribute("style", "text");
-    b2b.setAttribute("id", 'b2');
+    b2b.setAttribute("id", 'b2b');
     //SETS BOXES AS NUMBERS
     b1.setAttribute("type", "number");
     b1b.setAttribute("type", "number");
@@ -49,15 +49,15 @@ document.getElementById("submit").onclick = function () {
             //t1b = First of line of text B
             //b2b = Box 2 B
             //b2 = Box 2
-            document.body.appendChild(t1);
-            document.getElementById("t1").innerHTML = "X1";
+            document.body.appendChild(tx1);
+            document.getElementById("tx1").innerHTML = "X1";
             document.body.appendChild(b1);
             document.body.appendChild(br);
-            document.body.appendChild(t1b);
-            document.getElementById("t2").innerHTML = "Y1";
+            document.body.appendChild(ty1);
+            document.getElementById("ty1").innerHTML = "Y1";
             document.body.appendChild(b1b);
             //BELOW THIS SHOULD NOT BE CHANGED
-            document.getElementById("h10").innerHTML = "Refresh after solving, failure to refresh would cause issues.";
+            document.getElementByName("h10").innerHTML = "Refresh after solving, failure to refresh would cause issues.";
             }
     };
     
