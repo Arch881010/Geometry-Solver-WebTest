6710b9dd708f754267d55f3f2b45b275082d6680
Arch881010 <111789310+Arch881010@users.noreply.github.com>
2022-11-08 15:17:16 -0600

    undo

--- id.js
+++
@@ -1,3 +0,0 @@
-var ecrypt='8384jfjsagakjgadkgandg932rjadflagaasmsnvmnMNKJEFHJAJkjadfadf%*#(W&#(%U^^%$*&@*%@#)%*@#%_'
-var x ='@#%%*BJdhasogfdewq0)(*%RE_09a0dfjoasdf*($W&#*(%&#@%_)!_)!hasidijasdfoah```~~~iohafijufas-2842-4284-3258-45-35845236032946-3246-=2=34059320623==]adf[adsglasgasdg[asdgasdgasdga"""kd""sgn"kNMG""<MNMGkjasgasgnasgnkasdjogjaoeeoiajdvnsofjase2737&*(&*(&(r898q278w7#&$*&#(*&@("""*&@'
-const ecryptid = encrypt+x;
